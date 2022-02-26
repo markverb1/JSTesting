96e80677fc6e4dbee77783b3b7e4e385e59ee16d
markverb1 <markverb4@gmail.com>
2022-02-26 19:02:53 +0200

    homework

---
+++ hw/hw2.js
@@ -0,0 +1,10 @@
+var numeral = prompt("enter number")
+if (numeral < 0) {
+    alert("number negative")
+} else if (numeral < 100) {
+    alert("number less than 100")
+} else if (numeral < 1000) {
+    alert("number less than 1000")
+} else {
+    alert("number greater than 1000")
+}
