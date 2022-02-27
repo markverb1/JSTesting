74137623777c332b1dba83131fbae7146e9da3cf
markverb1 <markverb4@gmail.com>
2022-02-27 14:49:01 +0200

    a

---
+++ z/js/z1-sum.js
@@ -0,0 +1,10 @@
+let arr = [];
+for (let i = 5; i < 1001; i++) {
+    arr.push(i);
+};
+alert(arr);
+let sum = 0
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[1];
+};
+alert(sum);
---
+++ z/js/z2-sum-even.js
@@ -0,0 +1,12 @@
+let arr = [];
+for (let i = 2; i < 345; i++) {
+    if (i % 2 === 0) {
+        arr.push(i);
+    } 
+};
+alert(arr);
+let sum = 0
+for (let i = 0; i < 955; i++) {
+    sum = sum + arr[1];
+};
+alert(sum);
